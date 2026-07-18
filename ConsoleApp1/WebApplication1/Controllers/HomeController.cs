557178ea03177861b0a965eddb4de4dbf59c300d
Althea Vallejos <teyakuromi.99@gmail.com>
2026-07-18 11:08:46 +0800

    about me

--- ConsoleApp1/WebApplication1/Controllers/HomeController.cs
+++ ConsoleApp1/WebApplication1/Controllers/HomeController.cs
@@ -11,14 +11,12 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult AboutMe()
     {
-        return View();
-    }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        ViewData["Greeting"] = "I am Althea, currently pursuing a Bachelor of Science in Information Technology. My studies have introduced me to programming, databases, and system design, but what excites me most is exploring how technology can be applied in practical, everyday solutions. I enjoy learning about user interface design, creating flowcharts, and experimenting with different tools to improve workflows.";
+        ViewData["Goal"] = "My career goal is not to become a software developer, but rather to build a path where I can apply my IT knowledge in areas such as system analysis, project management, or technology support.I am passionate about helping people understand and use technology more effectively, and I see myself contributing to projects that improve efficiency and accessibility.Outside of academics, I enjoy creative problem?solving, collaborating with peers, and continuously expanding my skills to prepare for future opportunities in the IT field.";
+
+        return View();
     }
 }
